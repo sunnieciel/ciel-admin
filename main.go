4412792a34d7c0ff0d25be9dbaf37d5d421ce019
1211sciel <1211sciel@gmail.com>
2022-03-21 18:35:08 +0800

    1

--- main.go
+++ main.go
@@ -10,20 +10,3 @@ import (
 func main() {
 	cmd.Main.Run(gctx.New())
 }
-
-/*
-todo
-	- c_diary 日记表
-		- 属性
-			- aid 管理员ID
-			- title 标题
-			- level 级别  普通 稀有 传承 唯一 史诗 传说
-			- content 内容
-			- sign1
-			- sign2
-			- sign3
-			- sign4
-			- sign5
-			- sign6
-			- sign7
-*/
