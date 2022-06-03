68740cdf38c14e413f16c5ad4708c1c816d004e3
1211sciel <1211sciel@gmail.com>
2022-06-03 20:45:10 +0800

    d

--- resource/public/resource/js/app2.js
+++ resource/public/resource/js/app2.js
@@ -1,3 +1,58 @@
+jQuery.each(["put", "delete"], function (i, method) {
+    jQuery[method] = function (url, data, callback, type) {
+        if (jQuery.isFunction(data)) {
+            type = type || callback;
+            callback = data;
+            data = undefined;
+        }
+        return jQuery.ajax({
+            url: url,
+            type: method,
+            dataType: 'json',
+            data: data,
+            success: callback
+        });
+    };
+});
+//https://codeseven.github.io/toastr/demo.html
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
+function noticeOk(msg) {
+    toastr.options.timeOut = 2000;
+    toastr['success'](msg);
+}
+
+function noticeError(msg) {
+    toastr.options.timeOut = 5000;
+    toastr['error'](msg);
+}
+
+function noticeWarning(msg) {
+    toastr.options.timeOut = 5000;
+    toastr['warning'](msg);
+}
+
+function noticeInfo(msg) {
+    toastr.options.timeOut = 5000;
+    toastr['info'](msg);
+}
+
 function logout() {
     $.ajax({
         url: '/admin/logout', type: 'get', dataType: 'json', success: function (data) {
@@ -46,4 +101,7 @@ $(function () {
     })
 })
 
-
+function setDark(value) {
+    Cookies.set('dark', value)
+    location.reload()
+}
