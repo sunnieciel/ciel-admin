2564c4fc3c82bfc98fa033451940a54269d60c5e
1211sciel <1211sciel@gmail.com>
2022-06-29 17:03:33 +0800

    d

--- internal/cmd/sys_router.go
+++ internal/cmd/sys_router.go
@@ -1,9 +1,21 @@
 package cmd
 
 import (
+	"ciel-admin/internal/controller"
+	"ciel-admin/internal/service/sys"
 	"github.com/gogf/gf/v2/net/ghttp"
 )
 
 func registerGenFileRouter(s *ghttp.Server) {
 
+	s.Group("/thingRecord", func(g *ghttp.RouterGroup) {
+		g.Middleware(sys.AuthAdmin)
+		g.GET("/path", controller.ThingRecord.Path)
+		g.GET("/path/add", controller.ThingRecord.PathAdd)
+		g.GET("/path/edit/:id", controller.ThingRecord.PathEdit)
+		g.Middleware(sys.LockAction)
+		g.GET("/path/del/:id", controller.ThingRecord.Del)
+		g.POST("/post", controller.ThingRecord.Post)
+		g.POST("/put", controller.ThingRecord.Put)
+	})
 }
