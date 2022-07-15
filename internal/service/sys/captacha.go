cc30bac0f442054ca59abac062c7d820dd982e10
1211sciel <1211sciel@gmail.com>
2022-07-15 23:00:12 +0800

    d

--- internal/service/sys/captacha.go
+++ internal/service/sys/captacha.go
@@ -1,6 +1,8 @@
 package sys
 
-import "github.com/mojocn/base64Captcha"
+import (
+	"github.com/mojocn/base64Captcha"
+)
 
 var (
 	Store = base64Captcha.DefaultMemStore
@@ -10,10 +12,10 @@ func NewDriver() *base64Captcha.DriverString {
 	driver := new(base64Captcha.DriverString)
 	driver.Height = 60
 	driver.Width = 187
-	driver.NoiseCount = 5
+	driver.NoiseCount = 1
 	//driver.ShowLineOptions = base64Captcha.OptionShowSlimeLine
-	driver.Length = 5
-	driver.Source = "1234567890qwertyuipkjhgfdsazxcvbnm"
+	driver.Length = 4
+	driver.Source = "1234567890"
 	driver.Fonts = []string{"wqy-microhei.ttc", "3Dumb.ttf"}
 	return driver
 }
